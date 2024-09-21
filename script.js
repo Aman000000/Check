a161304ba620441e76945ff0ef2eecc27587dda2
Aman000000 <101815063+Aman000000@users.noreply.github.com>
2024-09-21 12:08:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventlistener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+
+        }
+
+
+        string += e.target.innerHTML;
+        input.value = string;
+
+
+    })
+})
